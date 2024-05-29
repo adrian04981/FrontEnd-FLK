31338c2388bdb6a9f93099e8b56f37a3855897a2
renekermit <renatocortex1516@gmail.com>
2024-05-28 19:55:02 -0500

    GESTIONSERVICIOS-CORTEZ

--- src/views/GestionarServicio/GestionarServicio.vue
+++ src/views/GestionarServicio/GestionarServicio.vue
@@ -1,31 +1,28 @@
 <template>
   <div class="container">
-    <!-- Lista de Servicios -->
     <div class="row justify-content-center mt-5">
       <div class="col-10">
-        <h2 class="text-center mb-4">Lista Servicios</h2>
+        <h2 class="text-center mb-4">Lista TipoServicio</h2>
         <div class="text-center mb-4">
-          <router-link to="/createservice" class="btn btn-primary">Crear Servicio</router-link>
+          <router-link to="/createuser" class="btn btn-primary">Crear Tipo Servicios</router-link>
         </div>
-        <div class="fallout-data-table">
+        <div class="colorful-data-table">
           <table class="table">
             <thead>
               <tr>
-                <th scope="col" class="text-white">PK_Servicio</th>
-                <th scope="col" class="text-white">FK_TipoServicio</th>
-                <th scope="col" class="text-white">Fecha Agendada</th>
-                <th scope="col" class="text-white">Acciones</th>
+                <th scope="col" class="text-dark">#</th>
+                <th scope="col" class="text-dark">Nombre</th>
+                <th scope="col" class="text-dark">Acciones</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="service in services" :key="service.PK_Servicio">
-                <td>{{ service.pkServicio }}</td>
-                <td>{{ service.fkTipoServicio }}</td>
-                <td>{{ service.fechaAgendada }}</td>
+              <tr v-for="TipoServicios in TipoServicio" :key="TipoServicios.PK_TiposServicio">
+                <td>{{ TipoServicios.pkTiposServicio }}</td>
+                <td>{{ TipoServicios.nombre }}</td>
                 <td>
-                  <b-button @click="showService(service)" variant="info">Consultar</b-button>
-                  <router-link :to="'/EditService/' + service.pkServicio" class="btn btn-warning mr-2">Editar</router-link>
-                  <router-link :to="'/DeleteService/' + service.pkServicio" class="btn btn-danger">Eliminar</router-link>
+                  <router-link :to="'/ViewUsuario/' + TipoServicios.pkTiposServicio" class="btn btn-info mr-2">Consultar</router-link>
+                  <router-link :to="'/Editusers/' + TipoServicios.pkTiposServicio" class="btn btn-warning mr-2">Editar</router-link>
+                  <router-link :to="'/DeleteUsuario/' + TipoServicios.pkTiposServicio" class="btn btn-danger">Eliminar</router-link>
                 </td>
               </tr>
             </tbody>
@@ -33,85 +30,133 @@
         </div>
       </div>
     </div>
-
-    <!-- Modal para Consultar Servicio -->
-    <b-modal v-if="selectedService" v-model="showModal" title="Consultar Servicio" @hide="clearSelectedService">
-      <div>
-        <p><strong>PK_Servicio:</strong> {{ selectedService.pkServicio }}</p>
-        <p><strong>FK_TipoServicio:</strong> {{ selectedService.fkTipoServicio }}</p>
-        <p><strong>Fecha Agendada:</strong> {{ selectedService.fechaAgendada }}</p>
+    <div class="row justify-content-center mt-5">
+      <div class="col-10">
+        <h2 class="text-center mb-4">Lista Servicios</h2>
+        <div class="text-center mb-4">
+          <router-link to="/createuser" class="btn btn-primary">Crear Servicios</router-link>
+        </div>
+        <div class="colorful-data-table">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col" class="text-dark">#</th>
+                <th scope="col" class="text-dark">FechaAgendada</th>
+                <th scope="col" class="text-dark">TipoServicio</th>
+                <th scope="col" class="text-dark">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="Servicios in Servicio" :key="Servicios.pkServicio">
+                <td>{{ Servicios.pkServicio }}</td>
+                <td>{{ Servicios.fechaAgendada }}</td>
+                <td>{{ Servicios.fkTipoServicio }}</td>
+                <td>
+                  <router-link :to="'/ViewUsuario/' + Servicios.pkServicio" class="btn btn-info mr-2">Consultar</router-link>
+                  <router-link :to="'/Editusers/' + Servicios.pkServicio" class="btn btn-warning mr-2">Editar</router-link>
+                  <router-link :to="'/DeleteUsuario/' + Servicios.pkServicio" class="btn btn-danger">Eliminar</router-link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </b-modal>
+    </div>
   </div>
 </template>
 
-<script>
+<script> 
 export default {
   name: 'Dashboard',
+  computed: {
+    isCurso() {
+      const role = localStorage.getItem('TipoServicio');
+      return role === 'Curso';
+    },
+    isInsp() {
+      const role = localStorage.getItem('TipoServicio');
+      return role === 'Inspeccion';
+    }
+  },
   data() {
     return {
-      services: [],
-      showModal: false,
-      selectedService: null
+      Servicio: [],
+      TipoServicio:[]
     };
   },
   mounted() {
-    this.fetchServices();
+    this.$axios.get('Servicios')
+      .then(response => {
+        this.Servicio = response.data;
+      })
+      .catch(error => {
+        console.error('Error al cargar datos de usuarios:', error);
+      });
+       // Obtiene la lista de roles al cargar el componente
+    this.$axios.get('TiposServicios')
+      .then(response => {
+        this.TipoServicio = response.data;
+      })
+      .catch(error => {
+        console.error('Error al cargar roles:', error);
+      });
   },
-  methods: {
-    fetchServices() {
-      this.$axios.get('Servicios')
-        .then(response => {
-          console.log('Datos de servicios:', response.data); // Log para verificar datos
-          this.services = response.data;
-        })
-        .catch(error => {
-          console.error('Error al cargar datos de servicios:', error);
-        });
-    },
-    showService(service) {
-      this.selectedService = service;
-      this.showModal = true;
-    },
-    clearSelectedService() {
-      this.selectedService = null;
-    }
   }
-};
 </script>
 
 <style scoped>
-.fallout-data-table {
+.colorful-data-table {
   font-family: 'Roboto', sans-serif;
-  color: #FFF;
+  color: #333;
   margin: 20px auto;
   padding: 20px;
-  background-color: #222;
-  border: 1px solid #888;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  background-color: #f7f7f7;
+  border: 2px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 }
 
-.fallout-data-table table {
+.colorful-data-table table {
   width: 100%;
   border-collapse: collapse;
 }
 
-.fallout-data-table th,
-.fallout-data-table td {
-  padding: 10px;
-  border-bottom: 2px solid #888;
+.colorful-data-table th,
+.colorful-data-table td {
+  padding: 15px;
+  border-bottom: 2px solid #ccc;
 }
 
-.fallout-data-table th {
-  background-color: #111;
+.colorful-data-table th {
+  background-color: #4caf50;
+  color: white;
 }
 
-.fallout-data-table tbody tr:nth-child(even) {
-  background-color: #333;
+.colorful-data-table tbody tr:nth-child(even) {
+  background-color: #f2f2f2;
 }
 
-.fallout-data-table tbody tr:hover {
-  background-color: #444;
+.colorful-data-table tbody tr:hover {
+  background-color: #ddd;
+}
+
+.btn-primary {
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+.btn-info {
+  background-color: #17a2b8;
+  border-color: #17a2b8;
+}
+
+.btn-warning {
+  background-color: #ffc107;
+  border-color: #ffc107;
+}
+
+.btn-danger {
+  background-color: #dc3545;
+  border-color: #dc3545;
 }
 </style>
