a2f79cafd8e231bed754b33ada90ab1b2c24eeb8
adrianh0498 <202011420@urp.edu.pe>
2024-06-09 23:42:29 -0500

    fix(dependencias): Se elimino la repeticion de depencias

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 5 CSS
-import 'bootstrap'; // Bootstrap 5 JS (bundle)
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'; // BootstrapVue Next CSS
-import '@fortawesome/fontawesome-free/css/all.min.css';  // Import Font Awesome
+
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Import BootstrapVue Next CSS
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+
+// Import Font Awesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// Import MDB Vue UI Kit CSS
 import 'mdb-vue-ui-kit/css/mdb.min.css';
 
 axios.defaults.baseURL = 'https://localhost:7006/api/';
